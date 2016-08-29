e0dff41174a9a55530e2e93246c7ea6a2ab13a7b
philip <philip.clarke@redhood.co.uk>
2016-08-29 12:14:41 +0100

    updated auth model

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,29 @@
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
+var Client = require('../models/client');
+
+passport.use('client-basic', new BasicStrategy(
+  function(username, password, callback) {
+    Client.findOne({
+      id: username
+    }, function(err, client) {
+      if (err) {
+        return callback(err);
+      }
+
+      // No client found with that id or bad password
+      if (!client || client.secret !== password) {
+        return callback(null, false);
+      }
+
+      // Success
+      return callback(null, client);
+    });
+  }
+));
+
+
 
 passport.use(new BasicStrategy(
   function(username, password, callback) {
@@ -36,5 +59,9 @@ passport.use(new BasicStrategy(
 ));
 
 exports.isAuthenticated = passport.authenticate('basic', {
+  session: false
+});
+
+exports.isClientAuthenticated = passport.authenticate('client-basic', {
   session: false
 });
