af1b8219b5a5724234623f8c6168ccfb5a7f6bb6
philip <philip.clarke@redhood.co.uk>
2016-08-30 14:57:13 +0100

    added new bearer strategy

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
 var Client = require('../models/client');
+var BearerStrategy = require('passport-http-bearer').Strategy
+var Token = require('../models/token');
 
 passport.use('client-basic', new BasicStrategy(
   function(username, password, callback) {
@@ -58,10 +60,49 @@ passport.use(new BasicStrategy(
   }
 ));
 
+passport.use(new BearerStrategy(
+  function(accessToken, callback) {
+    Token.findOne({
+      value: accessToken
+    }, function(err, token) {
+      if (err) {
+        return callback(err);
+      }
+
+      // No token found
+      if (!token) {
+        return callback(null, false);
+      }
+
+      User.findOne({
+        _id: token.userId
+      }, function(err, user) {
+        if (err) {
+          return callback(err);
+        }
+
+        // No user found
+        if (!user) {
+          return callback(null, false);
+        }
+
+        // Simple example with no scope
+        callback(null, user, {
+          scope: '*'
+        });
+      });
+    });
+  }
+));
+
 exports.isAuthenticated = passport.authenticate('basic', {
   session: false
 });
 
 exports.isClientAuthenticated = passport.authenticate('client-basic', {
+  session: false
+});
+
+exports.isBearerAuthenticated = passport.authenticate('bearer', {
   session: false
 });
