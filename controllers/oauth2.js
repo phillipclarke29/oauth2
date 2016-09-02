03cc8bb61f9fdd6659ff878e03ee586dc2136613
philip <philip.clarke@redhood.co.uk>
2016-09-02 14:25:00 +0100

    added deserialixation

--- controllers/oauth2.js
+++ controllers/oauth2.js
@@ -6,3 +6,20 @@ var Code = require('../models/code');
 
 // Create OAuth 2.0 server
 var server = oauth2orize.createServer();
+
+/ Register serialialization function
+server.serializeClient(function(client, callback) {
+  return callback(null, client._id);
+});
+
+// Register deserialization function
+server.deserializeClient(function(id, callback) {
+  Client.findOne({
+    _id: id
+  }, function(err, client) {
+    if (err) {
+      return callback(err);
+    }
+    return callback(null, client);
+  });
+});
